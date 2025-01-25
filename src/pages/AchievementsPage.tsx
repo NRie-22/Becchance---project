514a3fed1c436c0a95dde1b03f1ece1d1c7120ed
Nataliya Rieger <artgallery2018@gmx.ch>
2025-01-25 22:23:58 +0100

    Update AchievementsPage.tsx

--- src/pages/AchievementsPage.tsx
+++ src/pages/AchievementsPage.tsx
@@ -67,12 +67,7 @@ const AchievementsPage = () => {
         harum nobis, blanditiis dolorum possimus quas quasi voluptates
         reprehenderit temporibus.
       </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-        reiciendis commodi ex nesciunt iusto fugiat, itaque neque laudantium
-        veritatis vel harum nobis, blanditiis dolorum possimus quas quasi
-        voluptates reprehenderit temporibus.
-      </p>
+  
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
         reiciendis commodi ex nesciunt iusto fugiat, itaque neque laudantium
